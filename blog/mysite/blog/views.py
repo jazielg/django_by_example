7184b386e60aa39d65a864c59b97ab0722d592e3
jazielg <jazielgj1995@gmail.com>
2018-12-29 19:29:21 -0200

    adiciona, pesquisa no blog por titulo, conteudo e autor

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -6,12 +6,12 @@ from django.views.generic import ListView
 
 from .models import Post, Comment
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 
 from taggit.models import Tag
 
-from django.db.models import Count
+from django.db.models import Count, Q
 
 
 # class PostListView(ListView):
@@ -84,3 +84,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.filter(
+                Q(title__icontains=query) |
+                Q(body__icontains=query) |
+                Q(author__username__icontains=query))
+
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
