daeb68e34a8c3b02b88e221645ed02e123c9b725
jazielg <jazielgj1995@gmail.com>
2019-01-06 16:23:33 -0200

    adiciona, capitulo 8 - pagina admin orders customizada

--- myshop/myshop/orders/views.py
+++ myshop/myshop/orders/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import OrderItem
-from .forms import OrderCreateForm
+from .forms import OrderCreateForm, Order
 from cart.cart import Cart
 
 from .tasks import order_created
 
 from django.urls import reverse
+
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -29,3 +31,9 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
