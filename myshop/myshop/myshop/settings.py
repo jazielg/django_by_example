de6e9134777790dc43e88f2d58807157b4171d71
jazielg <jazielgj1995@gmail.com>
2019-01-08 22:39:45 -0200

    adiciona, capitulo 9 - interface de traducao rosetta, traducao da url, escolher idioma

--- myshop/myshop/myshop/settings.py
+++ myshop/myshop/myshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'paypal.standard.ipn',
     'payment.apps.PaymentConfig',
     'coupons.apps.CouponsConfig',
+    'rosetta',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- myshop/myshop/myshop/urls.py
+++ myshop/myshop/myshop/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
     path('paypal/', include('paypal.standard.ipn.urls')),
-    path('payment/', include('payment.urls', namespace='payment')),
-    path('coupons/', include('coupons.urls', namespace='coupons')),
+    path(_('payment/'), include('payment.urls', namespace='payment')),
+    path(_('coupons/'), include('coupons.urls', namespace='coupons')),
+    path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
