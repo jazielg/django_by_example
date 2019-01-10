a392242fb2b98ce364a119f544abbd08ebeab3f1
jazielg <jazielgj1995@gmail.com>
2019-01-10 12:44:43 -0200

    adiciona, enviar variavel em views para templates

--- myshop/myshop/shop/views.py
+++ myshop/myshop/shop/views.py
@@ -31,4 +31,5 @@ def product_detail(request, id, slug):
     r = Recommender()
     recommended_products = r.suggest_products_for([product], 4)
     return render(request, 'shop/product/detail.html', {'product': product,
-                                                        'cart_product_form': cart_product_form})
+                                                        'cart_product_form': cart_product_form,
+                                                        'recommended_products': recommended_products})
